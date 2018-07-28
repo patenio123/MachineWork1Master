058b2210679f595df89403784f1cd94fe57d3cbf
Jayson Patenio <jaysonpatenio28@gmail.com>
2018-07-27 18:46:19 -0700

    Add All Files

---
+++ app/src/main/java/edu/sti/myhomework/MyHomework.java
@@ -0,0 +1,13 @@
+package edu.sti.myhomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyHomework extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_homework);
+    }
+}
